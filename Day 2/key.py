c02de1f48f06dbaa3360ec5ccf6d16ed87736e73
ABHIRAM765 <94164367+ABHIRAM765@users.noreply.github.com>
2023-09-02 18:16:49 +0530

    Add files via upload

---
+++ Day 2/key.py
@@ -0,0 +1,13 @@
+a=[1,2,4,5,0]
+key1=4
+b = [11, 22, 33 ,44]
+key2 = 33
+if key1 in a:
+    print("key is found in a")
+else:
+    print("key not found in a")
+if key2 in b:
+    print("key is found in b")
+else:
+    print("key not found in b")
+
